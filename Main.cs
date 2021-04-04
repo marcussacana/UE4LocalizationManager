06285d6b730393bd7fe73186e0932ffa3d847eb5
Marcus André <ddtank.marcus@gmail.com>
2021-04-04 18:12:52 -0300

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,131 @@
+﻿using AdvancedBinary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UE4LocalizationManager
+{
+    public class LocRes
+    {
+        LocResFormat Resource;
+        byte[] Script;
+        public LocRes(byte[] Script) {
+            this.Script = Script;
+        }
+
+
+        public string[] Import() {
+            using (var Stream = new MemoryStream(Script))
+            using (var Reader = new StructReader(Stream, Encoding: Encoding.Unicode)) {
+                Reader.ReadStruct(ref Resource);   
+            }
+            return (from x in Resource.Localizations select x.Content).ToArray();
+        }
+
+        public byte[] Export(string[] Strings) {
+            if (Resource.Localizations.Length != Strings.Length)
+                throw new Exception("You can't add/remove strings from a locres file");
+            
+            for (int i = 0; i < Resource.Localizations.Length; i++) {
+                Resource.Localizations[i].Content = Strings[i];
+            }
+
+            using (var Stream = new MemoryStream())
+            using (var Writer = new StructWriter(Stream, Encoding: Encoding.Unicode)) {
+                Writer.WriteStruct(ref Resource);
+                Writer.Flush();
+                return Stream.ToArray();
+            }
+        }
+    }
+
+#pragma warning disable 649, 169
+    struct LocResFormat {
+        [FArray(0x10)]
+        public byte[] Signature;
+        public byte Version;
+
+        public ulong StringTableOffset;
+
+        [StructField, PArray]
+        public Group[] Groups;
+
+        [StructField, PArray]
+        public PString[] Localizations;
+    }
+
+    struct Group {
+        [StructField]
+        PString Name;
+
+        [StructField, PArray]
+        GroupKey[] Keys;
+    }
+
+    struct GroupKey {
+        [StructField]
+        PString Name;
+
+        uint Hash;
+        uint LocId;
+    }
+
+    struct PString
+    {
+
+        [PreProcess("PreProcessString"), PostProcess("PostProcessString")]
+        public int Length;
+
+
+        [Ignore]
+        Encoding EntryEncoding;
+
+        [Ignore]
+        public string Content;
+
+
+        //Encode the Length Field Before Write
+        FieldInvoke PreProcessString => new FieldInvoke((Strm, Reading, BigEndian, Struct) =>
+        {
+            if (Reading)
+                return Struct;
+
+            byte[] Data = Struct.EntryEncoding.GetBytes(Struct.Content);
+            Struct.Length = Data.Length;
+
+            if (Struct.EntryEncoding.EncodingName == "Unicode")
+            {
+                Struct.Length /= 2;
+                Struct.Length ^= 0xFFFFFFFF;
+            }              
+
+            return Struct;
+        });
+
+        FieldInvoke PostProcessString => new FieldInvoke((Strm, Reading, BigEndian, Struct) =>
+        {
+            if (Reading)
+            {
+                bool Unicode = Struct.Length < 0;
+                Struct.EntryEncoding = Struct.Length < 0 ? Encoding.Unicode : Encoding.ASCII;
+                if (Struct.Length < 0)
+                    Struct.Length ^= 0xFFFFFFFF;
+
+                byte[] Buff = new byte[Unicode ? (Struct.Length + 1) * 2 : (Struct.Length + 1)];
+                Strm.Read(Buff, 0, Buff.Length);
+                Struct.Content = Struct.EntryEncoding.GetString(Buff).TrimEnd('\x0');
+            }
+            else
+            {
+                byte[] Data = Struct.EntryEncoding.GetBytes(Struct.Content + '\x0');
+                Strm.Write(Data, 0, Data.Length);
+            }
+            return Struct;
+        });
+    }
+#pragma warning restore 649, 169
+}
---
+++ ULMGUI/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UE4LocalizationManager;
+
+namespace ULMGUI
+{
+    public partial class Form1 : Form
+    {
+        LocRes Localization;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = listBox1.Items[listBox1.SelectedIndex].ToString();
+            }
+            catch { }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            var Data = System.IO.File.ReadAllBytes(openFileDialog1.FileName);
+            Localization = new LocRes(Data);
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(Localization.Import());
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r' || e.KeyChar == '\n')
+            {
+                listBox1.Items[listBox1.SelectedIndex] = textBox1.Text.Replace("\\n", "\n");
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            var Data = Localization.Export(listBox1.Items.Cast<string>().ToArray());
+            System.IO.File.WriteAllBytes(saveFileDialog1.FileName, Data);
+            MessageBox.Show("Saved.");
+        }
+    }
+}
